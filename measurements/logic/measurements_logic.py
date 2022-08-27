9ffe28ff9ee31a339f31c9d3da3d228a55080f29
JuanCamFalla <78000303+JuanCamFalla@users.noreply.github.com>
2022-08-27 10:04:38 -0500

    Reyes no sabe seguir instrucciones

--- measurements/logic/measurements_logic.py
+++ measurements/logic/measurements_logic.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from tkinter import Variable
 from ..models import Measurement  
 
 def get_measurements():
@@ -10,12 +12,24 @@ def get_measurement(var_pk):
 
 def update_measurement(var_pk, new_var):
     measurement = get_measurement(var_pk)
-    measurement.name = new_var["name"]
+    measurement= Measurement(
+        variable= new_var["variable"],
+        value= new_var["value"],
+        place = new_var["place"],
+        date= datetime.now()
+    )
     measurement.save()
     return measurement
 
 def create_measurement(var):
-    measurement = measurement(name=var["name"])
+    variable = Variable.objects.get(pk=var["variable"])
+    measurement = Measurement(
+        variable= variable,
+        value = var["value"],
+        unit = var["unit"],
+        place = var["place"],
+        date = datetime.now()
+    )
     measurement.save()
     return measurement
 
@@ -23,4 +37,3 @@ def delete_measurement(var_pk):
     measurement = get_measurement(var_pk)
     measurement.delete()
     return measurement
-
